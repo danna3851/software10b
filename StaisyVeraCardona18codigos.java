73f7f87f0e40d6861eefcba9452d394d0db2cd6a
Staisy2978 <staisy_vera2978@liceonuevoshorizontes.edu.co>
2025-07-24 21:19:16 -0500

    Add files via upload

---
+++ StaisyVeraCardona18codigos.java
@@ -0,0 +1,109 @@
+
+package com.mycompany.staisysena;
+
+/**
+ *
+ * @author RLX-PC
+ */
+public class StaisySena {
+
+    public static void main(String[] args) {
+            String Name="Staisy Vera";
+    String name="Ashly Vargas";
+   
+    System.out.println(Name);
+    System.out.println(name);
+   
+   
+    int a= 22;
+    int b= 23;
+    int suma=a+b;
+    System.out.println("el valor de la suma de a+b="+" "+ suma);
+   
+    int s= 23;
+    int t= 22;
+    int resta=s-t;
+    System.out.println("el valor de la resta de s-t="+" "+ resta);
+   
+    int v= 22;
+    int c= 23;
+    int multiplicación=v*c;
+    System.out.println("el valor de la multiplicación de v*c="+" "+ multiplicación);
+   
+    double e1= 28;
+    double e2= 32;
+    double división=e1/e2;
+    System.out.println("el valor de la división de e1/e2="+" "+ división);
+   
+    int e= 13;
+    int alcuadrado= e*e;
+    System.out.println("el valor de e al cuadrado="+" "+ alcuadrado);
+   
+     double gradosC=(2);
+     double gradosF= ((gradosC)*(1.8)+32);
+     System.out.println("2 grados Celsius en Fahrenheit es: " + gradosF);
+       int B = 8;
+    int altura = 3;
+    int perimetro = 2 * (B + altura);
+    System.out.println("El perímetro del rectángulo es: " + perimetro);
+
+    double radio = 8;
+    double area = (3.14159 *radio * radio);
+    System.out.println("El área del círculo es: " + area);
+
+    int años = 40;
+    int dias = años * 365;
+    System.out.println("37 años equivalen a " + dias + " días.");
+
+    int T = 20;
+    int doble = T * 2;
+    int triple = T * 3;
+    System.out.println("El doble de " + T + " es: " + doble);
+    System.out.println("El triple de " + T + " es: " + triple);
+
+    int km = 25;
+    int metros = km * 1000;
+    int centimetros = km * 100000;
+    System.out.println("25 km son " + metros + " metros.");
+    System.out.println("25  km son " + centimetros + " centimetros.");
+
+    int lado = 8;
+    int area1 = lado * lado;
+    int perimetro1 = lado+lado+lado+lado;
+    System.out.println("Área del cuadrado: " + area1);
+    System.out.println("Perímetro del cuadrado: " + perimetro1);
+
+    int lado1 = 5;
+    int volumen = lado1 * lado1 * lado1;
+    System.out.println("El volumen del cubo es: " + volumen);
+
+    double radio2 = 20;
+    double volumen1 = (4 / 3) * 3.14159* radio2*radio2*radio2;
+    System.out.println("El volumen de la esfera es: " + volumen1);
+
+    int X= 1;
+    int A= 2;
+    int Z= 3;
+    int suma1 = X+ A+ Z;
+    System.out.println("La suma de los tres primeros números naturales es: " + suma1);
+
+    int precio = 80000;
+    int porciento = 25;
+    int descuento = precio * porciento / 100;
+    System.out.println("El descuento de $" + precio + " con " + porciento + "% es: $" + descuento);
+
+    double total = 500000;
+    double porcentaje1 = 50;
+    double propina = total * porcentaje1 / 100;
+ 
+    System.out.println("La propina de $" + total + " con " + porcentaje1 + "% es: $" + propina);
+    }
+}
+    
+  
+    
+    
+    
+    
+    
+    
